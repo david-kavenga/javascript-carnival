d5f236467fd47a6168463fe8a67b5063ddda4fd0
david-kavenga <david.kavenga@gmail.com>
2022-03-03 13:34:23 +1300

    add mole movement to whack a mole

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -10,68 +10,89 @@ let hitCount = 0
 let highScore = 0
 let challenge = 0
 
-// set up Audio
-const swipeAudio = new Audio('whack-audio.wav')
-swipeAudio.volume = 0.25
+// Set up Audio
+const hitAudio = new Audio('whack-audio.wav')
+hitAudio.volume = 0.25
+const missAudio = new Audio(
+	'223489__mrgreaper__bunnynernernanerner.ogg'
+)
+missAudio.volume = 0.25
 
-// add event listeners an populate cellsArr
+// Add event listeners an populate cellsArr
 for (let td of cells) {
 	td.addEventListener('click', whackedMole)
 	cellsArr.push({ element: td, isMole: false })
 }
-
 document
 	.getElementById('startChallenge')
-	.addEventListener('click', start)
+	.addEventListener('click', startChallenge)
 
 // Place initial mole
-placeMole(0)
+let previousMole = placeMole(0)
+
+// Set move timer
+moveInterval = setInterval(function () {
+	previousMole = placeMole(previousMole)
+}, 2000)
 
 function whackedMole(evt) {
+	// Get clicked cell
 	const cell = cellsArr.find(
 		(entry) => entry.element == evt.target.closest('td')
 	)
 
+	// Whack and place mole
 	if (cell.isMole == true) {
-		swipeAudio.currentTime = 0
-		swipeAudio.play()
-		placeMole(cell)
+		hitAudio.currentTime = 0
+		hitAudio.play()
+		if (challenge) hitCount++
+		previousMole = placeMole(previousMole)
+	}
+	// Missed!
+	else {
+		missAudio.currentTime = 0
+		missAudio.play()
 	}
 }
 
 function placeMole(prev) {
+	// Remove old element
 	if (prev != 0) {
-		if (challenge) hitCount++
-		prev.isMole = false
-		let cells = prev.element.children
-		prev.element.removeChild(cells[0])
+		deMolify(prev)
 	}
 
-	let pos = Math.floor(Math.random() * cellsArr.length)
-	cellsArr[pos].isMole = true
-	const img = cellsArr[pos].element.appendChild(
-		document.createElement('img')
+	// Choose a different cell index
+	let pos = chooseCell(
+		cellsArr.indexOf(prev),
+		cellsArr.length
 	)
-	img.src = 'mole.PNG'
-	img.style.width = '74px'
-	img.style.height = '71px'
 
+	// Make the cell a mole
+	molify(cellsArr[pos])
+
+	// Update the hit count
 	document.getElementById('count').innerText = hitCount
+
+	return cellsArr[pos]
 }
 
-function start() {
+function startChallenge() {
+	// Switch to challenge mode
 	challenge = true
 	let mode = document.getElementById('gameType')
 	mode.innerText = '--Challenge Mode-- 10'
 
+	// Set game timer
 	let timeLeft = 10
 	let gameTimer = setInterval(function () {
 		if (timeLeft == 0) {
+			// Switch to practice mode and update scores
 			challenge = false
 			clearInterval(gameTimer)
 			mode.innerText = '--Practice Mode--'
 			updateScores()
 		} else {
+			// Count down
 			timeLeft--
 			mode.innerText = '--Challenge Mode-- ' + timeLeft
 		}
@@ -79,13 +100,50 @@ function start() {
 }
 
 function updateScores() {
+	let alertMsg = 'Times Up! Score: ' + hitCount
+
+	// New High Score
 	if (hitCount > highScore) {
 		highScore = hitCount
-		alert('Times Up! Score: ' + hitCount)
 		document.getElementById('highScore').innerText =
 			highScore
+		alertMsg += '\nNEW HIGH SCORE!'
 	}
+
+	// Game end alert
+	alert(alertMsg)
 
 	hitCount = 0
 	document.getElementById('count').innerText = hitCount
+}
+
+function chooseCell(prevInd, length) {
+	if (prevInd == 'undefined')
+		return Math.floor(Math.random() * length)
+
+	// generate new random index until it isn't the old cell location
+	let pos = prevInd
+	while (pos == prevInd) {
+		pos = Math.floor(Math.random() * length)
+	}
+
+	return pos
+}
+
+function molify(cell) {
+	// Put mole in a cell
+	cell.isMole = true
+	const img = cell.element.appendChild(
+		document.createElement('img')
+	)
+	img.src = 'mole.PNG'
+	img.style.width = '74px'
+	img.style.height = '71px'
+}
+
+function deMolify(cell) {
+	// Remove mole from a cell
+	cell.isMole = false
+	let children = cell.element.children
+	cell.element.removeChild(children[0])
 }
